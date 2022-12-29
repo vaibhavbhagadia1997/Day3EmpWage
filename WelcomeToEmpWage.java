7e233f0e571b8a16f3635b63c41c0f2b0e3eab79
vaibhavbhagadia@1999 <vaibhavbhagadia68@gmail.com>
2022-12-29 12:35:18 +0530

    Added: greeting

---
+++ WelcomeToEmpWage.java
@@ -0,0 +1,10 @@
+package EmpWageComputation;
+
+public class WelcomeToEmpWage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("WELCOME TO EMPLOYEE WAGE");
+	}
+
+}
